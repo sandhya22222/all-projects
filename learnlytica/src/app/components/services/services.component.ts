fde0a0747129a0973941d4f948995604b557fe12
sandhya <allu.sandhya025@gmail.com>
2022-04-12 14:41:27 +0530

    added life cycle hook

--- learnlytica/src/app/components/services/services.component.ts
+++ learnlytica/src/app/components/services/services.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChange } from '@angular/core';
+
 
 @Component({
   selector: 'app-services',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
+  data = 'THBS';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(`new - data is ${this.data}`);
+  }
+  ngOnChanges(changes: SimpleChange) {
+    console.log(`ngOnChanges - data is ${this.data}`);
   }
 
+  ngOnInit() {
+    console.log(`ngOnInit - data is ${this.data}`);
+  }
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+  ngContentChecked() {
+    console.log("ngContentChecked");
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
 }
