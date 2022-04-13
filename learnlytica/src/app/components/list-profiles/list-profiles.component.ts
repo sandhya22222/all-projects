c3f8ab250dc383201ee96f5c75d1266a9a8d8b56
sandhya <allu.sandhya025@gmail.com>
2022-04-13 12:20:04 +0530

    added rxjs observable using service

--- learnlytica/src/app/components/list-profiles/list-profiles.component.ts
+++ learnlytica/src/app/components/list-profiles/list-profiles.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+
+import { UserService } from 'src/app/services/user.service';
 
 
 export interface PeriodicElement {
@@ -9,16 +12,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -27,13 +30,38 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./list-profiles.component.css']
 })
 export class ListProfilesComponent implements OnInit {
+
+  myUserList: any;
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
+  userdata: any;
 
-  constructor( ) { }
+  constructor(private myconfig: UserService) { }
 
   ngOnInit(): void {
-   
+    this.myconfig.getUserList()
+
+      .pipe(
+        //rxjs operater -> going to hold data
+
+        tap((userList: any) => {
+          return userList.map((
+            element: any, index: number, arr: any
+          ) => {
+            element['title'] = 'Disney';
+            element['name'] = "Mr ." + [element.name];
+          })
+        })
+
+      )
+
+
+      .subscribe(
+        (data) => {
+          this.myUserList = data
+        });
+
   }
 
 }
---
+++ learnlytica/src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private myHttp:HttpClient) { }
+
+  getUserList() {
+    return this.myHttp.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
+}
+}
