47af4397388e4f72d4d53aad15e20d8587752eaf
sandhya <allu.sandhya025@gmail.com>
2022-04-14 13:42:50 +0530

    added .env file and call that in to app.js

--- todo-node/app.js
+++ todo-node/app.js
@@ -1,20 +1,23 @@
-const http =require('http');
+require('dotenv').config();
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const webServer = http.createServer (
-    (req ,res) =>{
-        console.log (req.headers);
-        console.log ("================");
-
-        res.statusCode =200;
-        res.setHeader('Content-Type' , 'text/plain');
-        res.setHeader('thbs-type','angular');
+const webServer = http.createServer(
+    (req, res) => {
+        // console.log(req.headers);
+        console.log("================");
+        console.log('hello world');
+        console.log(process.env.NAME);
+        console.log(process.env.BATCH);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('thbs-type', 'angular');
         res.end('Hello World');
     }
 )
 
-webServer.listen(port,hostname,()=>{
-    console.log (`Server running at http://${hostname}:${port}/`);
+webServer.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 })
