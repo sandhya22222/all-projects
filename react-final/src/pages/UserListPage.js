b45a4a941177a76fc02366aa713c0d1a320f080c
sandhya <allu.sandhya025@gmail.com>
2022-05-20 10:23:57 +0530

    sending API call using react

--- react-final/src/pages/UserListPage.js
+++ react-final/src/pages/UserListPage.js
@@ -1,23 +1,44 @@
-import logo from '../logo.svg';
 
+import { useEffect, useState } from 'react';
 
 export default function UserListPage(props) {
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    return (
-
-        <div className='row'>
-            <div className='col-3'>
-                <div className="card" >
-                    <img src={logo} width="100" height={100} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+    useEffect(
+        () => {
+            fetch('https://reqres.in/api/users?page=1')
+                .then(res => res.json())
+                .then(
+                    (success) => {
+                        setUsers(success.data);
+                        setIsLoaded(true)
+                    },
+                    (error) => {
+                        setError(error)
+                    }
+                )
+        }
     )
 
+
+    return (
+        <ul>
+            <div className='row'>
+                {users.map(item => (
+                    <div key={item.id} className='col-3'>
+                        <div className="card">
+                            <img src={item.avatar} width="100" height={190} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{item.first_name} {item.last_name}</h5>
+                                <p className="card-text"><b>Email:</b>{item.email}</p>
+                                <a className="btn btn-primary">Click Here for More Details</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </ul>
+    )
 }
--- react-final/src/pages/UserPage.js
+++ react-final/src/pages/UserPage.js
@@ -1,31 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default class UserPage extends React.Component {
-
-    constructor() {
-        super();
-
+    constructor(props) {
+        super(props);
         this.state = {
-            uname: 'THBS',
-            age: 20,
-            addr: 'BNG'
+            users: [],
+            erros: null,
+            isLoaded: false
         }
     }
 
-    updateAddress() {
-        this.setState({
-            addr: 'HYD'
-        })
+    //sending api call life cycle hook
+
+    componentDidMount() {
+        fetch('https://reqres.in/api/users?page=1')
+            .then(res => res.json())
+            .then(
+                (success) => {
+                    this.setState({
+                        isLoaded: true,
+                        users: success.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error: error
+                    });
+                }
+            )
     }
 
     render() {
+        const { users, isLoaded, error } = this.state;
+
         return (
-            <div>
-                User Name :  {this.state.uname} <br></br>
-                User Age :  {this.state.age} <br></br>
-                User addr :  {this.state.addr} <br></br>
-            </div>
+            <ul>
+                {users.map(item => (
+                    <li key={item.id}>
+                      Name:  {item.first_name} {item.last_name}
+                    </li>
+                ))}
+            </ul>
         )
     }
+
+
+
+
+
+
+    // constructor() {
+    //     super();
+
+    //     this.state = {
+    //         uname: 'THBS',
+    //         age: 20,
+    //         addr: 'BNG'
+    //     }
+    // }
+
+    // updateAddress() {
+    //     this.setState({
+    //         addr: 'HYD'
+    //     })
+    // }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             User Name :  {this.state.uname} <br></br>
+    //             User Age :  {this.state.age} <br></br>
+    //             User addr :  {this.state.addr} <br></br>
+    //         </div>
+    //     )
+    // }
 
 }
